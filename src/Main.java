c206943a89f9c08639ec365ccbb8199d6926c958
ThePythonGamer <haydenrayne.rooney@gmail.com>
2020-10-28 10:29:42 -0400

    Adding Commenting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
+        //Start programming!
         System.out.println("Hello World!");
         System.out.println("So you are saying if I change this...");
     }
