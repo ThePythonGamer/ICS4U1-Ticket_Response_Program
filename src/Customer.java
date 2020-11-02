935e1faa91e9a606e8d4bbe389db85ac3b770d17
lifavo <ea.zchaos@gmail.com>
2020-11-02 10:21:32 -0500

    Added Part of the JavaDocs.

--- src/Customer.java
+++ src/Customer.java
@@ -8,7 +8,14 @@ public class Customer {
     private int VIPTicket;
     private String response;
 
-
+    /**This is a Customer object that represent a customer with email and his/her request on standard ticket, vip ticket.
+     * A response is included for emailing this customer later.
+     *
+     * @param email - this represent the customer's email
+     * @param standardTicket - this represent the customer's request for standard ticket
+     * @param VIPTicket - this represent the customer's request for vip ticket
+     * @param response - this is the response to give for the customer's request.
+     */
     public Customer(String email, int standardTicket, int VIPTicket, String response) {
         this.email = email;
         this.standardTicket = standardTicket;
@@ -16,26 +23,49 @@ public class Customer {
         this.response = response;
     }
 
+    /**This get email of the Customer object
+     *
+     * @return email of the customer
+     */
     public String getEmail() {
         return email;
     }
 
+    /**This returns the amount of standard ticket being requested by the customer
+     *
+     * @return amount of standard ticket being requested
+     */
     public int getStandardTicket() {
         return standardTicket;
     }
-
+    /**This returns the amount of VIP ticket being requested by the customer
+     *
+     * @return amount of VIP ticket being requested
+     */
     public int getVIPTicket() {
         return VIPTicket;
     }
 
+    /** This returns the response for the customer's request
+     *
+     * @return information for email response
+     */
     public String getResponse() {
         return response;
     }
 
+    /**This set the response for the customer's request
+     *
+     * @param response - information for email response to be changed
+     */
     public void setResponse(String response) {
         this.response = response;
     }
 
+    /**Override toString
+     *
+     * @return Customer's information in one sentence
+     */
     @Override
     public String toString() {
         return "Customer with email " + email + " requested " +
--- src/OrderQueue.java
+++ src/OrderQueue.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
  *
  * @version 1
  */
+
+
 public class OrderQueue {
+    /**This is a queue for storing every customer's information in a certain order.
+     */
     private ArrayList<Customer> Queue = new ArrayList<>();
 
+    /**
+     * This add a Customer object to the queue
+     * @param toAdd - Customer object to be added
+     */
     public void enqueue(Customer toAdd) {
         Queue.add(toAdd);
     }
 
+    /**This removes the first object in the queue and return the removed element
+     *
+     * @return the first Customer object in the queue, if there is no element to return, return null
+     */
     public Customer dequeue() {
         try {
             return Queue.remove(0);
@@ -19,10 +31,18 @@ public class OrderQueue {
         }
     }
 
+    /**
+     * This return the size of the queue
+     * @return the size of the queue
+     */
     public int size() {
         return Queue.size();
     }
 
+    /**Get the first element of the queue
+     *
+     * @return the first Customer object, if there is no element to return, return null
+     */
     public Customer peek(){
         try {
             return Queue.get(0);
